8a6c34b492b613d9c4b6b7124c44565d3d9ee69a
Rubin99 <rubinshrestha14@gmail.com>
2020-12-05 20:34:34 +0545

    git commit -a -m

--- ASE_Comp1/Form1.Designer.cs
+++ ASE_Comp1/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel_Draw = new System.Windows.Forms.Panel();
+            this.txt_MultiCmd = new System.Windows.Forms.TextBox();
+            this.txt_Cmd = new System.Windows.Forms.TextBox();
+            this.Run = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel_Draw
+            // 
+            this.panel_Draw.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel_Draw.Location = new System.Drawing.Point(38, 33);
+            this.panel_Draw.Name = "panel_Draw";
+            this.panel_Draw.Size = new System.Drawing.Size(421, 373);
+            this.panel_Draw.TabIndex = 0;
+            this.panel_Draw.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Draw_Paint);
+            this.panel_Draw.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel_Draw_MouseClick);
+            // 
+            // txt_MultiCmd
+            // 
+            this.txt_MultiCmd.Location = new System.Drawing.Point(485, 33);
+            this.txt_MultiCmd.Multiline = true;
+            this.txt_MultiCmd.Name = "txt_MultiCmd";
+            this.txt_MultiCmd.Size = new System.Drawing.Size(291, 229);
+            this.txt_MultiCmd.TabIndex = 1;
+            // 
+            // txt_Cmd
+            // 
+            this.txt_Cmd.Location = new System.Drawing.Point(485, 291);
+            this.txt_Cmd.Name = "txt_Cmd";
+            this.txt_Cmd.Size = new System.Drawing.Size(291, 22);
+            this.txt_Cmd.TabIndex = 2;
+            // 
+            // Run
+            // 
+            this.Run.Location = new System.Drawing.Point(588, 344);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(75, 23);
+            this.Run.TabIndex = 3;
+            this.Run.Text = "Run";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.txt_Cmd);
+            this.Controls.Add(this.txt_MultiCmd);
+            this.Controls.Add(this.panel_Draw);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel_Draw;
+        private System.Windows.Forms.TextBox txt_MultiCmd;
+        private System.Windows.Forms.TextBox txt_Cmd;
+        private System.Windows.Forms.Button Run;
     }
 }
 
--- ASE_Comp1/Form1.cs
+++ ASE_Comp1/Form1.cs
@@ -16,5 +16,20 @@ namespace ASE_Comp1
         {
             InitializeComponent();
         }
+
+        private void panel_Draw_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel_Draw_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
